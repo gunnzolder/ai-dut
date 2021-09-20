9ff4925b1e4a343515572a8761a7bee655a739c3
Anton Parkhomenko <anton@merge.rocks>
2021-09-20 23:32:31 +0300

    added link parser

--- lab-1/wikiParsers.js
+++ lab-1/wikiParsers.js
@@ -1,7 +1,17 @@
+// FULL NAMES
 var src = document.querySelectorAll('#mw-pages .mw-category .mw-category-group li a'),
     result = [];
 src.forEach(el => {
     result.push(el.innerHTML);
+});
+
+console.log(result);
+
+// WIKI LINKS
+var src = document.querySelectorAll('#mw-pages .mw-category .mw-category-group li a'),
+    result = [];
+src.forEach(el => {
+    result.push(el.getAttribute('href'));
 });
 
 console.log(result);
